17ecefd7f78d9ff7a14ef8e57a1a8e876bcb7913
Scott Tang <fq600@yahoo.com>
2018-08-31 14:59:33 -0500

    done

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,34 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    this.name = name;
+    this.crew = crew;
+    this.phasers = phasers;
+    this.shields = shields;
+    this.cloaked = false;
+    this.warpDrive = "disengaged";
+    this.phasersCharge = "uncharged";
+    crew.length ? (this.docked = false) : (this.docked = true);
+    this.notifyCrew();
+  }
+
+  notifyCrew() {
+    this.crew.forEach(crewMember => (crewMember.currentShip = this));
+  }
+}
+
+scott = new CrewMember("Pilot");
+pablo = new CrewMember("Gunner");
+kevin = new CrewMember("Defender");
+
+fireFalcon = new Spaceship(
+  "Fire Falcon",
+  [scott, kevin, pablo],
+  "laser beams",
+  "Ultron"
+);
+// console.log(fireFalcon.notifyCrew());
+
+// console.log(scott);
+// console.log(fireFalcon);
+console.log(fireFalcon.docked);
+console.log(scott.currentShip);
